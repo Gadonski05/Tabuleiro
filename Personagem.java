0fcb03702955a55090959b08b82275a623982442
Matheus Henrique Rodrigues Gadonski <142113295+Gadonski05@users.noreply.github.com>
2024-04-18 20:09:30 -0300

    Persona
    Criação da class Personagem, para tirar as variáveis e os outros elementos da Main principal do código, deixando tudo separado.

---
+++ Personagem.java
@@ -0,0 +1,18 @@
+public class Personagem {
+    
+    private int posx;
+    private int posy;
+    String desenho;
+
+    public int getPox(){
+
+        return posx;
+
+    }
+
+    public int getPoy(){
+
+        return posy;
+
+    }
+}
