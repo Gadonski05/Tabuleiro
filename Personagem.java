7ada4aefe0c88d70ad64761f38cba2eaf87c0275
Matheus Henrique Rodrigues Gadonski <142113295+Gadonski05@users.noreply.github.com>
2024-04-18 20:45:00 -0300

    Declarando
    Colocando as class para que a class funcione corretamente.

--- Personagem.java
+++ Personagem.java
@@ -1,9 +1,17 @@
 public class Personagem {
     
+    String nome;
+    private Color cor;
     private int posx;
     private int posy;
     String desenho;
 
+    public Personagem(String nome, Color cor, int posx, int posy){
+        this.nome = nome;
+        this.cor = cor;
+        this.posx = posx;
+        this.posy = posy;
+    }
     public int getPox(){
 
         return posx;
